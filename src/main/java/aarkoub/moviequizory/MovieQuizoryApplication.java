5e0937d9bb87de8d1d993ff5df873de665a98de0
amel <aarkoub@github.com>
2021-06-11 18:31:05 +0200

    First commit
    Revert "First commit"
    
    This reverts commit b1a64dd7

---
+++ src/main/java/aarkoub/moviequizory/MovieQuizoryApplication.java
@@ -0,0 +1,13 @@
+package aarkoub.moviequizory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieQuizoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieQuizoryApplication.class, args);
+	}
+
+}
