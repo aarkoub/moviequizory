dae8a23767f474fb44d7975ac61a55f321d15b36
amel <aarkoub@github.com>
2021-06-14 12:28:36 +0200

    Add quiz generator [domain] #SB2

---
+++ src/main/java/aarkoub/moviequizory/domain/quiz/generator/QuizGenerator.java
@@ -0,0 +1,53 @@
+package aarkoub.moviequizory.domain.quiz.generator;
+
+import aarkoub.moviequizory.domain.actor.Actor;
+import aarkoub.moviequizory.domain.movie.Movie;
+import aarkoub.moviequizory.domain.question.Question;
+import aarkoub.moviequizory.domain.quiz.Quiz;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class QuizGenerator {
+
+    Random rand;
+    int maxSizeQuiz = 10;
+
+    public QuizGenerator(){
+        rand = new Random();
+        rand.setSeed(123456789);
+    }
+
+    public QuizGenerator(int maxSizeQuiz){
+        this.maxSizeQuiz = maxSizeQuiz;
+        new QuizGenerator();
+    }
+
+    public Quiz generate(Map<Integer, Movie> moviesMap, Map<Integer, List<Actor>> actorsInMovieMap){
+        Object[] movieIds = moviesMap.keySet().toArray();
+        Question[] questions = new Question[maxSizeQuiz];
+        for (int i = 0; i < maxSizeQuiz; i++) {
+            int ind = rand.nextInt() & Integer.MAX_VALUE % movieIds.length;
+            Movie movie = moviesMap.get(movieIds[ind]);
+            boolean isTrue = rand.nextBoolean();
+            Actor actor;
+            if (isTrue) {
+                List<Actor> actors = actorsInMovieMap.get(movieIds[ind]);
+                actor = actors.get(rand.nextInt() & Integer.MAX_VALUE % actors.size());
+            } else {
+                int otherMovieInd = rand.nextInt() & Integer.MAX_VALUE % movieIds.length;
+                while (otherMovieInd == ind) {
+                    otherMovieInd = rand.nextInt() & Integer.MAX_VALUE % movieIds.length;
+                }
+                List<Actor> actors = actorsInMovieMap.get(movieIds[otherMovieInd]);
+                actor = actors.get(rand.nextInt() & Integer.MAX_VALUE % actors.size());
+            }
+            questions[i] = new Question(actor, movie, isTrue);
+        }
+
+        return new Quiz(questions);
+    }
+
+
+}
---
+++ src/test/java/aarkoub/moviequizory/domain/quiz/generator/QuizGeneratorTest.java
@@ -0,0 +1,91 @@
+package aarkoub.moviequizory.domain.quiz.generator;
+
+import aarkoub.moviequizory.domain.actor.Actor;
+import aarkoub.moviequizory.domain.movie.Movie;
+import aarkoub.moviequizory.domain.quiz.Quiz;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class QuizGeneratorTest {
+
+    @Test
+    public void generate() {
+        QuizGenerator gen = new QuizGenerator();
+        Map<Integer, Movie> moviesMap = new HashMap<>();
+        Map<Integer, List<Actor>> actorsInMovies = new HashMap<>();
+
+        moviesMap.put(1, new Movie("abc", null));
+        moviesMap.put(2, new Movie("def", null));
+
+        List<Actor> a1 = new ArrayList<>();
+        a1.add(new Actor("harry", null));
+        a1.add(new Actor("hermione", null));
+
+        List<Actor> a2 = new ArrayList<>();
+        a2.add(new Actor("ron", null));
+        a2.add(new Actor("pettigrew", null));
+
+        actorsInMovies.put(1, a1);
+        actorsInMovies.put(2, a2);
+
+        Quiz quiz = gen.generate(moviesMap, actorsInMovies);
+
+        Assertions.assertEquals(10, quiz.getQuestions().length);
+
+        for (int i = 0; i < quiz.getQuestions().length; i++) {
+
+            System.out.println(quiz.getQuestions()[i] + " : " + quiz.getQuestions()[i].isTrue());
+
+            switch (quiz.getQuestions()[i].getActor().getName()) {
+                case ("harry"):
+                    switch (quiz.getQuestions()[i].getMovie().getTitle()) {
+                        case ("abc"):
+                            Assertions.assertTrue(quiz.getQuestions()[i].isTrue());
+                            break;
+                        case ("def"):
+                            Assertions.assertFalse(quiz.getQuestions()[i].isTrue());
+                            break;
+                    }
+                    break;
+                case ("hermione"):
+                    switch (quiz.getQuestions()[i].getMovie().getTitle()) {
+                        case ("abc"):
+                            Assertions.assertTrue(quiz.getQuestions()[i].isTrue());
+                            break;
+                        case ("def"):
+                            Assertions.assertFalse(quiz.getQuestions()[i].isTrue());
+                            break;
+                    }
+                    break;
+                case ("ron"):
+                    switch (quiz.getQuestions()[i].getMovie().getTitle()) {
+                        case ("abc"):
+                            Assertions.assertFalse(quiz.getQuestions()[i].isTrue());
+                            break;
+                        case ("def"):
+                            Assertions.assertTrue(quiz.getQuestions()[i].isTrue());
+                            break;
+                    }
+                    break;
+                case ("pettigrew"):
+                    switch (quiz.getQuestions()[i].getMovie().getTitle()) {
+                        case ("abc"):
+                            Assertions.assertFalse(quiz.getQuestions()[i].isTrue());
+                            break;
+                        case ("def"):
+                            Assertions.assertTrue(quiz.getQuestions()[i].isTrue());
+                            break;
+                    }
+                    break;
+            }
+
+        }
+
+    }
+
+}
