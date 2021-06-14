6bdfd50167e010cfad6ad64c0aa97fbcac9a5630
amel <aarkoub@github.com>
2021-06-14 14:26:49 +0200

    Add api #SB2 #SB4
    Note that tests don't mock the whole process (routes, service, repo, db)
    TODO: add tests that test only api routes

---
+++ src/main/java/aarkoub/moviequizory/MovieQuizoryAPI.java
@@ -0,0 +1,82 @@
+package aarkoub.moviequizory;
+
+import aarkoub.moviequizory.db.user.UserNotFoundException;
+import aarkoub.moviequizory.domain.quiz.Quiz;
+import aarkoub.moviequizory.domain.quiz.service.IQuizService;
+import aarkoub.moviequizory.domain.user.User;
+import aarkoub.moviequizory.domain.user.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@SpringBootApplication
+public class MovieQuizoryAPI {
+
+	@Autowired
+	IUserService userService;
+
+	@Autowired
+	IQuizService quizService;
+
+	@PostMapping("/users/create")
+	@ResponseBody
+	void createUser(HttpServletRequest request, HttpServletResponse response){
+		Cookie cookie = new Cookie("userId", userService.retrieve(getUserId(request)).getId().toString());
+		response.addCookie(cookie);
+	}
+
+	@GetMapping("/users/{id}")
+	@ResponseBody
+	void getUser(HttpServletRequest request, HttpServletResponse response){
+		Cookie cookie = new Cookie("userId", userService.retrieve(getUserId(request)).getId().toString());
+		response.addCookie(cookie);
+	}
+
+	@PostMapping("/users/{id}/setHighscore")
+	@ResponseBody
+	User setHighscoreUser(@PathVariable String id, @RequestBody Map<String, Integer> score,
+						  HttpServletRequest request, HttpServletResponse response){
+		try {
+			return userService.setHighscore(UUID.fromString(id), score.get("new_highscore"));
+		} catch (UserNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+	}
+
+	@GetMapping("/quizzes/generate")
+	@ResponseBody
+	Quiz generateQuiz(HttpServletRequest request, HttpServletResponse response){
+		return quizService.getQuiz();
+	}
+
+	public @Nullable
+	UUID getUserId(HttpServletRequest request){
+		UUID userId = null;
+		Cookie[] cookies = request.getCookies();
+		if(cookies!=null) {
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().equals("userId")) {
+					userId = UUID.fromString(cookies[i].getValue());
+					break;
+				}
+			}
+		}
+		return userId;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieQuizoryAPI.class, args);
+	}
+
+}
---
+++ src/test/java/aarkoub/moviequizory/MovieQuizoryAPITests.java
@@ -0,0 +1,78 @@
+package aarkoub.moviequizory;
+
+import aarkoub.moviequizory.domain.question.Question;
+import aarkoub.moviequizory.domain.user.User;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.Cookie;
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@AutoConfigureTestEntityManager
+@Transactional
+class MovieQuizoryAPITests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void createUser() throws Exception {
+        mockMvc.perform(get("/users/create"))
+                .andExpect(status().isOk())
+                .andExpect(cookie().exists("userId"));
+    }
+
+    @Test
+    void getUser() throws Exception {
+
+        UUID id = testEntityManager.persist(new User()).getId();
+        Cookie cookie = new Cookie("userId", id.toString());
+        mockMvc.perform(get("/users/" + id).cookie(cookie))
+                .andExpect(status().isOk())
+                .andExpect(cookie().value("userId", id.toString()));
+    }
+
+    @Test
+    void setHighscore() throws Exception {
+
+        UUID id = testEntityManager.persist(new User()).getId();
+        Cookie cookie = new Cookie("userId", id.toString());
+        mockMvc.perform(post("/users/" + id + "/setHighscore")
+                .cookie(cookie)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"new_highscore\":8}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(id.toString())))
+                .andExpect(jsonPath("$.highscore", is(8)));
+    }
+
+    @Test
+    void generateQuiz() throws Exception {
+
+        mockMvc.perform(get("/quizzes/generate"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.questions", Matchers.hasSize(10)));
+    }
+
+}
