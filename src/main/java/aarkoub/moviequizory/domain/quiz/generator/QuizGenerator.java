93db68193837f1a03a6651fc4a7e7bf88babb322
amel <aarkoub@github.com>
2021-06-16 22:11:24 +0200

    Fix random ind in quiz generation [domain]#SB2

--- src/main/java/aarkoub/moviequizory/domain/quiz/generator/QuizGenerator.java
+++ src/main/java/aarkoub/moviequizory/domain/quiz/generator/QuizGenerator.java
@@ -28,22 +28,21 @@ public class QuizGenerator {
     public Quiz generate(Map<Integer, Movie> moviesMap, Map<Integer, List<Actor>> actorsInMovieMap){
         Object[] movieIds = moviesMap.keySet().toArray();
         Question[] questions = new Question[maxSizeQuiz];
-        System.out.println(movieIds.length);
         for (int i = 0; i < maxSizeQuiz; i++) {
-            int ind = rand.nextInt() & Integer.MAX_VALUE % movieIds.length;
+            int ind = Math.abs(rand.nextInt()) % movieIds.length;
             Movie movie = moviesMap.get(movieIds[ind]);
             boolean isTrue = rand.nextBoolean();
             Actor actor;
             if (isTrue) {
                 List<Actor> actors = actorsInMovieMap.get(movieIds[ind]);
-                actor = actors.get(rand.nextInt() & Integer.MAX_VALUE % actors.size());
+                actor = actors.get(Math.abs(rand.nextInt()) % actors.size());
             } else {
-                int otherMovieInd = rand.nextInt() & Integer.MAX_VALUE % movieIds.length;
+                int otherMovieInd = Math.abs(rand.nextInt()) % movieIds.length;
                 while (otherMovieInd == ind) {
-                    otherMovieInd = rand.nextInt() & Integer.MAX_VALUE % movieIds.length;
+                    otherMovieInd = Math.abs(rand.nextInt()) % movieIds.length;
                 }
                 List<Actor> actors = actorsInMovieMap.get(movieIds[otherMovieInd]);
-                actor = actors.get(rand.nextInt() & Integer.MAX_VALUE % actors.size());
+                actor = actors.get(Math.abs(rand.nextInt()) % actors.size());
             }
             questions[i] = new Question(actor, movie, isTrue);
         }
