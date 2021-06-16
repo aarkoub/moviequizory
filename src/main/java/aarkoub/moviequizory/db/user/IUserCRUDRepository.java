8f99f7dc4be4664fb258aace339ea07c54315f53
amel <aarkoub@github.com>
2021-06-16 23:01:13 +0200

    Add transactional for user crud repo [db]

--- src/main/java/aarkoub/moviequizory/db/user/IUserCRUDRepository.java
+++ src/main/java/aarkoub/moviequizory/db/user/IUserCRUDRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Repository
+@Transactional
 public interface IUserCRUDRepository extends CrudRepository<User, UUID> {
 
     @Modifying(clearAutomatically = true)
